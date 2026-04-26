31e161d52b3e586fde8ce1580dc8252392ac5b0e
AndreCastr0 <andrecsleite@gmail.com>
2026-04-26 19:12:41 +0000

    feat: adicionar leitura do sensor ultrassonico

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
-from machine import Pin, PWM
+from machine import Pin, PWM, time_pulse_us
 from time import sleep_ms
 
+TRIG_PIN = 5
+ECHO_PIN = 18
 SERVO_PIN = 4
 
+trig = Pin(TRIG_PIN, Pin.OUT)
+echo = Pin(ECHO_PIN, Pin.IN)
 servo = PWM(Pin(SERVO_PIN), freq=50)
 
 
@@ -13,13 +17,29 @@ def mover_servo(angulo):
     servo.duty(duty)
 
 
-while True:
-    for angulo in [0, 45, 90, 135, 180]:
-        mover_servo(angulo)
-        print("Servo no angulo:", angulo)
-        sleep_ms(700)
+def medir_distancia_cm():
+    trig.value(0)
+    sleep_ms(2)
 
-    for angulo in [135, 90, 45]:
-        mover_servo(angulo)
-        print("Servo no angulo:", angulo)
-        sleep_ms(700)
+    trig.value(1)
+    sleep_ms(1)
+    trig.value(0)
+
+    duracao = time_pulse_us(echo, 1, 30000)
+
+    if duracao < 0:
+        return None
+
+    distancia = (duracao * 0.0343) / 2
+    return distancia
+
+
+while True:
+    distancia = medir_distancia_cm()
+
+    if distancia is None:
+        print("Distancia: erro")
+    else:
+        print("Distancia:", round(distancia, 1), "cm")
+
+    sleep_ms(700)
