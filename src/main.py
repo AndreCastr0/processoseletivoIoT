800083d0221722147531f6873947016eba744d32
AndreCastr0 <andrecsleite@gmail.com>
2026-04-26 18:41:51 +0000

    feat: adicionar controle inicial do servo

--- src/main.py
+++ src/main.py
@@ -1,1 +1,25 @@
-print("Teste")
+from machine import Pin, PWM
+from time import sleep_ms
+
+SERVO_PIN = 4
+
+servo = PWM(Pin(SERVO_PIN), freq=50)
+
+
+def mover_servo(angulo):
+    duty_min = 26
+    duty_max = 123
+    duty = int(duty_min + (angulo / 180) * (duty_max - duty_min))
+    servo.duty(duty)
+
+
+while True:
+    for angulo in [0, 45, 90, 135, 180]:
+        mover_servo(angulo)
+        print("Servo no angulo:", angulo)
+        sleep_ms(700)
+
+    for angulo in [135, 90, 45]:
+        mover_servo(angulo)
+        print("Servo no angulo:", angulo)
+        sleep_ms(700)
